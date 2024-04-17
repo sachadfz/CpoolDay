135b979e6a6eed2348f2e6df5983c48bc37d051e
sachadfz <167294295+sachadfz@users.noreply.github.com>
2024-04-17 12:50:03 +0000

    Exercice 2

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -2,13 +2,9 @@ void print_char(char c);
 void print_number(int value);
 void my_print_n_ascii(int howMany);
 
-int main(void) {
-    my_print_n_ascii(5);
-    return 0;
-}
-void my_print_n_ascii(int howMany)
+void my_print_n_ascii (int howMany)
 {
-    for (int a=33; a<38; a=a+1 ) {
+    for (int a=33; a<howMany+33; a=a+1 ) {
     char c=a;
     print_char(c);
     }
